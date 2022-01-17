a7373f6c32cead394bfd6ef09692956b878e1bdb
EC2 Default User <ec2-user@ip-172-31-21-167.ec2.internal>
2022-01-17 09:17:49 +0000

    [FINISH]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,10 @@ class BooksController < ApplicationController
     if @book.save
       # リダイレクトと同時に”Book was successfully created.”を表示。
       flash[:notice] = 'Book was successfully created.'
-      redirect_to show_book_path
+      redirect_to show_book_path(@book.id)
     else
-      flash.now[:alert]
-      render : index
+      @books = Book.all
+      render :index
     end
   end
   
@@ -26,16 +26,21 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to books_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to show_book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
    @book = Book.find(params[:id])
    @book.destroy #destroyメソッドを使用し対象のツイートを削除する。
   # リダイレクトと同時に'Book was successfully destroyed'を表示。
-   redirect_to books_path, alert: 'Book was successfully destroyed.'
+   redirect_to books_path
+  flash[:notice] = 'Book was successfully destroyed.'
   end
   
   private
